f314c3c03db0e55dbb2bf846051c611926f838b7
saraporri <saraporricino@yahoo.com>
2023-11-13 18:45:03 +0100

    Created dictionary component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import Dictionary from "./Dictionary";
+
 import "./App.css";
 
 export default function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+      </div>{" "}
+      <footer className="App-footer">Coded by Sara</footer>
+    </div>
+  );
 }
