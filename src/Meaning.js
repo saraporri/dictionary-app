a390cdf77c40029012bc77fb567ced51ea685b77
saraporri <saraporricino@yahoo.com>
2023-11-14 20:11:15 +0100

    Rendered the word synonyms

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,13 +1,16 @@
 import React from "react";
+import Synonyms from "./Synonyms";
 export default function Meaning(props) {
   console.log(props.meaning);
   return (
     <div className="Meaning">
       <h3>{props.meaning.partOfSpeech}</h3>
       <p>
-        {props.meaning.definition}
+        Definition: {props.meaning.definition}
         <br />
-        <em>{props.meaning.example}</em>
+        Example: <em>{props.meaning.example}</em>
+        <br />
+        <Synonyms synonyms={props.meaning.synonyms} />
       </p>
     </div>
   );
