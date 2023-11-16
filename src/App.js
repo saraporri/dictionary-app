a0c6b309d0b0b79f8dd99b517521211e81a6df8d
saraporri <saraporricino@yahoo.com>
2023-11-16 13:42:48 +0100

    Fixed style

--- src/App.js
+++ src/App.js
@@ -7,11 +7,17 @@ export default function App() {
     <div className="App">
       <div className="container">
         <main>
-          <Dictionary defaultKeyword="music" />
+          <Dictionary defaultKeyword="universe" />
         </main>
       </div>{" "}
       <footer className="App-footer">
-        <a href="https://github.com/saraporri/dictionary-app">Code</a> by Sara
+        <a href="https://github.com/saraporri/dictionary-app">
+          Open-source code
+        </a>
+        <span> by </span>
+        <a href="https://www.linkedin.com/in/sara-p-665919103/">
+          Sara Porricino
+        </a>
       </footer>
     </div>
   );
