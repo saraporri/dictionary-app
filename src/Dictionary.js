85cec778b96a905aecda3c134a5c8d5a24aeb81c
saraporri <saraporricino@yahoo.com>
2023-11-16 12:38:46 +0100

    Created photos section and styled

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
-  function handleResponse(response) {
-    console.log(response.data.meanings);
+  let [photos, setPhotos] = useState(null);
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+  function handleImagesResponse(response) {
+    console.log(response.data);
+    setPhotos(response.data.photos);
   }
   function search() {
     //https://www.shecodes.io/learn/apis/dictionary
-    let apiKey = "8b07dt8e17fee9f438eo54a03e745179";
+    const apiKey = "8b07dt8e17fee9f438eo54a03e745179";
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    //https://www.shecodes.io/learn/apis/images
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${apiKey}&per_page=6`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleSubmit(event) {
@@ -45,6 +54,7 @@ export default function Dictionary(props) {
           <div className="hint">Suggested words: book, sunset, travel...</div>
         </section>
         <Results results={results} />
+        <Photos photos={photos} />
       </div>
     );
   } else {
